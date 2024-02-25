0f9d2fe5ed328c4e8db5d5b6024a5581a55fb035
Maxuel Vieira Tobá Junior <maxueltstz@hotmail.com>
2024-02-25 10:27:08 -0300

    começando microsservicos

---
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/dto/PagamentoDTO.java
@@ -0,0 +1,22 @@
+package br.com.alurafood.pagamentos.dto;
+
+import java.math.BigDecimal;
+
+import br.com.alurafood.pagamentos.model.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PagamentoDTO {
+	
+	private Long id;
+	private BigDecimal valor;
+	private String nome;
+	private String numero;
+	private String expiracao;
+	private String codigo;
+	private Status status;
+	private Long pedidoId;
+	private Long formaDePagamentoId;
+}
